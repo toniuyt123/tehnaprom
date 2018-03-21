edd34da7a824b62c644d0076c1badefb0dcb0ecc
StefotoG <stefan.grozev@yahoo.com>
2018-03-21 13:22:12 +0200

    luda rabota

--- B_02_Antonio_Milev/app/models/cinamas_film.rb
+++ B_02_Antonio_Milev/app/models/cinamas_film.rb
@@ -1,4 +1,10 @@
 class CinamasFilm < ApplicationRecord
 	belongs_to :film
 	belongs_to :cinema
+
+
+	validates :film_id, uniqueness: { scope: :cinema_id }
+	#oops! these validates weren't necassary
+	#validates :film_id, inclusion: {in: Film.all.map{|f| f.id}, message: "Film does not exist"}
+	#validates :cinema_id, inclusion: {in: Cinema.all.map{|c| c.id}, message: "Cinema does not exist"}
 end
